993c5ec36b4fb6b197841690b36127fdb8d82b7e
Wil89 <wilber@fonoma.com>
2022-09-29 12:52:32 +0200

    actualizar y eliminar videos

--- controllers/VideosController.php
+++ controllers/VideosController.php
@@ -10,9 +10,57 @@ class VideosController
         echo json_encode($videos);
     }
 
-    public function find($id)
+    public function detail($id)
     {
         $video = Video::find($id);
         echo json_encode($video);
+    }
+
+    public function create()
+    {
+        // parsear la data del post
+        $data = json_decode(file_get_contents("php://input"));
+        if (!$data) {
+            // Bad Request
+            http_response_code(400);
+            exit;
+        }
+        $video = new Video($data->name, $data->imagenUrl, $data->videoUrl, $data->postDate);
+        $video->save();
+        echo json_encode($video);
+    }
+
+    public function update($id)
+    {
+        $data = json_decode(file_get_contents("php://input"));
+        if (!$data) {
+            // Bad Request
+            http_response_code(400);
+            exit;
+        }
+        $video = Video::find($id);
+        if ($video) {
+            $video->name = $data->name;
+            $video->imagenUrl = $data->imagenUrl;
+            $video->videoUrl = $data->videoUrl;
+            $video->postDate = $data->postDate;
+            $video->save();
+            $response = $video;
+            echo json_encode($response);
+        } else {
+            http_response_code(400);
+            exit;
+        }
+    }
+
+    public function delete($id) {
+        $video = Video::find($id);
+        if(!$video) {
+            http_response_code(400);
+            exit;
+        }
+        $video->remove();
+        $response = "Video eliminado";
+        echo json_encode($response);
     }
 }
--- models/Video.php
+++ models/Video.php
@@ -8,6 +8,14 @@ class Video extends DB {
     public $videoUrl;
     public $postDate;
 
+    public function __construct($name, $imagenUrl, $videoUrl, $postDate)
+    {
+        $this->name = $name;
+        $this->imagenUrl = $imagenUrl;
+        $this->videoUrl = $videoUrl;
+        $this->postDate = $postDate;
+    }
+
     public static function all()
     {
         $db = new DB();
@@ -22,6 +30,38 @@ class Video extends DB {
         $prepare = $db->prepare("SELECT * FROM Video WHERE id=:id");
         $prepare->execute([":id" => $id]);
         return $prepare->fetchObject(Video::class);
+    }
+
+    public function save()
+    {
+        $params = [
+            ":name"=> $this->name, 
+            ":postDate" => $this->postDate, 
+            ":imagenUrl" => $this->imagenUrl,
+            ":videoUrl" => $this->videoUrl,
+        ];
+        // Si no esta presente el id, es que estamos creando un nuevo video
+        if (empty($this->id)) {
+            $prepare = $this->prepare("INSERT INTO Video(name, imagenUrl, videoUrl, postDate) VALUES (:name, :imagenUrl, :videoUrl, :postDate)");
+            $prepare->execute($params);
+            $prepare2 = $this->prepare("SELECT MAX(id) id FROM Video");
+            $prepare2->execute();
+            $this->id = $prepare2->fetch(PDO::FETCH_ASSOC)["id"];
+        } else {
+            //  Si esta presente el id, entonces es un update
+            $params[":id"] = $this->id;
+            $prepare = $this->prepare("UPDATE Video SET name=:name, imagenUrl=:imagenUrl, videoUrl=:videoUrl, postDate=:postDate WHERE id=:id");
+            $prepare->execute($params);
+        } 
+        
+
+
+    }
+
+    public function remove()
+    {
+        $prepare=$this->prepare("DELETE FROM Video WHERE id=:id");
+        $prepare->execute([":id" => $this->id]);
     }
 
 }
