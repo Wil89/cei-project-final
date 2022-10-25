da1b3ac881fcf191272c472daa83cbb3b4279beb
Wil89 <wilber@fonoma.com>
2022-10-25 16:12:18 +0200

    validation for upload video modal

--- views/js/crearVideoModal.js
+++ views/js/crearVideoModal.js
@@ -2,6 +2,7 @@ let modalContainer = null;
 
 const formBuscar = document.querySelector("#buscarVideo");
 const inputBuscar = document.querySelector("#buscarVideoInput");
+
 formBuscar.addEventListener("submit", (event) => {
   event.preventDefault();
   const url = `/videos/index/${inputBuscar.value}`;
@@ -31,12 +32,13 @@ crearVideoModal = () => {
                             type="text"
                             class="form-control" name="url" id="videoUrl" required placeholder="Titulo del video">
                         <label for="nombre">Video URL</label>
+                        <p id="error-msg"></p>
                     </div>
                 </form>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary btn-subir" data-bs-dismiss="modal">Subir Video</button>
+                <button type="button" class="btn btn-primary btn-subir">Subir Video</button>
             </div>
         </div>
     </div>
@@ -46,6 +48,10 @@ crearVideoModal = () => {
   let modal = new bootstrap.Modal(document.getElementById("createModal"));
   modal.show();
 
+  const modalValidacion = document.querySelector("#error-msg");
+  // Inicialización
+  modalValidacion.style.display = "none";
+
   // Mensaje Toast
   const toast = document.querySelector("#alert-toast");
   const toastBody = document.querySelector(".toast-body");
@@ -54,45 +60,62 @@ crearVideoModal = () => {
   const videoUrl = document.querySelector("#videoUrl");
 
   const actionBtn = document.querySelector(".btn-subir");
+
+  // Escuchar evento de click para subir video
   actionBtn.addEventListener("click", () => {
     const toastMsg = new bootstrap.Toast(toast);
-    const postDate = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    const postDate = new Date().toISOString().slice(0, 19).replace("T", " ");
 
-    const url = videoUrl.value.split("src=")[1].split("title=")[0].trim();
-    const title = videoUrl.value
-      .split("title=")[1]
-      .split("frameborder=")[0]
-      .trim();
+    //Validar que no este vació el video iframe pegado o no coincida con una etiqueta válida de un iframe
+    if (!videoUrl.value) {
+      modalValidacion.style.display = "block";
+      modalValidacion.style.color = "red";
+      modalValidacion.innerHTML = "Introduzca un video"
+    } else if (
+      !videoUrl.value.match(/(?:<iframe[^>]*)(?:(?:\/>)|(?:>.*?<\/iframe>))/)
+    ) {
+      modalValidacion.style.display = "block";
+      modalValidacion.style.color = "red";
+      modalValidacion.innerHTML = "El valor introducido no es una etiqueta iframe válida"
+      // return;
+    } else {
+      modal.hide();
+      const url = videoUrl.value.split("src=")[1].split("title=")[0].trim();
+      const title = videoUrl.value
+        .split("title=")[1]
+        .split("frameborder=")[0]
+        .trim();
 
-    const data = {
-      name: title.substring(1, title.length - 1),
-      videoUrl: videoUrl.value,
-      postDate: postDate,
-    };
+      const data = {
+        name: title.substring(1, title.length - 1),
+        videoUrl: videoUrl.value,
+        postDate: postDate,
+      };
 
-    console.log(data);
-    fetch("/videos/create", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          // Recargar la pagina para mostrar las nuevas entradas
-          window.location.href = "/videos";
-          toastBody.innerHTML = "Video subido satisfactoriamente.";
-          toastMsg.show();
-        } else if (response.status === 400) {
-          // Si el status code es 400 es q ya tenemos ese video en la DB
-          // alert("Video Repetido");
-          toastBody.innerHTML = "<p>El Video ya existe.</p>";
-          toastMsg.show();
-          return;
-        }
+      console.log(data);
+      fetch("/videos/create", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
       })
-      .catch((error) => console.log(error));
+        .then((response) => {
+          if (response.ok) {
+            // Recargar la pagina para mostrar las nuevas entradas
+            window.location.href = "/videos";
+            toastBody.innerHTML = "Video subido satisfactoriamente.";
+            toastMsg.show();
+          } else if (response.status === 400) {
+            // Si el status code es 400 es q ya tenemos ese video en la DB
+            // alert("Video Repetido");
+            toastBody.innerHTML = "<p>El Video ya existe.</p>";
+            toastMsg.show();
+            return;
+          }
+        })
+        .catch((error) => console.log(error));
+    }
   });
 };
 
@@ -110,7 +133,7 @@ const addComment = (id) => {
 
   const data = {
     comment: commentInput.value,
-    date: new Date().toISOString().slice(0, 19).replace('T', ' '),
+    date: new Date().toISOString().slice(0, 19).replace("T", " "),
     videoId: parseInt(id),
     userId: 2,
   };
