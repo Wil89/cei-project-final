f9bef79dc148713da795fad2b5c67337bf00064f
Wil89 <wilber@fonoma.com>
2022-10-31 15:21:37 +0100

    add favicon

--- views/videos/index.php
+++ views/videos/index.php
@@ -4,7 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Videos</title>
+  <title>Video Blog</title>
+  <link rel="shortcut icon" href="/public/images/vlog.png" type="image/x-icon">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="views/css/videos.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
